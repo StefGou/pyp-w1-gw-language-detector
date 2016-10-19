aba3c9b74b2dfc121087edca940873dbe7185ab9
Stéphan Gouin <sgouin@gmail.com>
2016-10-18 20:21:12 -0400

    cleaned up the code

--- language_detector/main.py
+++ language_detector/main.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-"""This is the entry point of the program."""
-
 def detect_language(text, languages):
     
     globalcount = 0
@@ -14,26 +12,10 @@ def detect_language(text, languages):
             
             if word in dictionary['common_words']:
                 count += 1
-                
-            
-        #print(dictionary['name'], count)
+                    
         if count > globalcount:
             globalcount = count
             lang = dictionary['name']
     
     
     return lang
-
-text = """
-            # spanish
-            Lionel Andrés Messi Cuccittini (Rosario, 24 de junio de 1987),
-            conocido como Leo Messi, es un futbolista argentino11 que juega
-            como delantero en el Fútbol Club Barcelona y en la selección
-            argentina, de la que es capitán.
-
-            # german
-            Messi spielt seit seinem 14. Lebensjahr für den FC Barcelona.
-            Mit 24 Jahren wurde er Rekordtorschütze des FC Barcelona, mit 25
-            der jüngste Spieler in der La-Liga-Geschichte, der 200 Tore
-            erzielte.
-        """
